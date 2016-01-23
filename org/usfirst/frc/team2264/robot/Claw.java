84c301241384cb310933d07796975eaef8e57e54
Nathan Ringo (Robotics) <ringonat000+robotics@isd284.com>
2016-01-23 09:10:49 -0600

    Sarah & Drake take 1

--- org/usfirst/frc/team2264/robot/Claw.java
+++ org/usfirst/frc/team2264/robot/Claw.java
@@ -18,7 +18,15 @@ public class Claw {
 	 * @param limitSwitch The switch which is triggered if the claw is about to open or close too far.
 	 */
 	public Claw(CANTalon horizontal, CANTalon vertical, DigitalInput limitSwitch) {
-		// TODO
+	
+		this.horizontal = horizontal;
+		this.vertical = vertical;
+		this.limitSwitch = limitSwitch;
+		this.lastHorizontalDirection = HorizontalDirection.STOP;
+		//constructors giving values to each instance variables 
+		//CANTalon horizontal is called horizontal, CANTalon vertical is called vertical
+		//DigitalInput is called limitSwitch, HorizontalDirection is called lastHorizontalDirection
+		
 	}
 
 	/**
@@ -28,6 +36,23 @@ public class Claw {
 	 */
 	public void setHorizontalDirection(HorizontalDirection direction, boolean safety) {
 		// TODO
+		/* If the safety is on and the limit switch is activated and we're still
+		 * trying to go further in the same direction disable the robot.
+		 * 
+		 * Otherwise, set the horizontal direction as normal.
+		 */
+		if (((limitSwitch.get() && safety) && (direction == lastHorizontalDirection))) {
+			this.horizontal.disableControl();
+		}
+		else {
+			this.horizontal.set(direction.getSpeed());
+		}
+		this.lastHorizontalDirection = direction;
+		//If safety and the limit switch is turned on and the arm is still trying to move in the
+		//direction the robot is disabled
+		//otherwise the direction is set back to the normal speed
+		// Calls variable horizontal. The set sets the speed of the horizontal motor, sets the speed
+		//to the passed in direction
 	}
 
 	/**
@@ -36,23 +61,50 @@ public class Claw {
 	 * @param safety Whether safety measures are enabled or not.
 	 */
 	public void setHorizontalDirection(int pov, boolean safety) {
-		// TODO
+		
+		if ((180 < pov) && (pov <= 359)) {
+		this.setHorizontalDirection(HorizontalDirection.OPEN, safety);
+		}
+		else if ((0 < pov) && (pov <= 179)) {
+		this.setHorizontalDirection(HorizontalDirection.CLOSED, safety);
+		}
+		else {
+		this.setHorizontalDirection(HorizontalDirection.STOP, safety);
+		}
 	}
+		//if the pov is pressed on the left side the arm is to open
+		//if the pov is pressed on the right side the arm is to close
+		//if the pov is not pressed at the certain angles it is to stop
 
 	/**
 	 * Sets the vertical motor to move in the requested direction.
 	 * @param direction The direction to move in.
 	 */
 	public void setVerticalDirection(VerticalDirection direction) {
-		// TODO
+		
+		this.vertical.set(direction.getSpeed());
+		// Calls variable vertical. Sets the speed of the vertical motor to the passed in direction
+		//sets the vertical direction as normal.
 	}
 	/**
 	 * Sets the vertical motor to move in the requested direction.
 	 * @param pov The POV angle to choose a direction from.
 	 */
 	public void setVerticalDirection(int pov) {
-		// TODO
+
+		if ((270 < pov) || (pov <= 89)) {
+			this.setVerticalDirection(VerticalDirection.UP);
+		}
+		else if ((90 < pov) && (pov <= 269)) {
+			this.setVerticalDirection(VerticalDirection.DOWN);
+		}
+		else {
+			this.setVerticalDirection(VerticalDirection.STOP);
+		}
 	}
+	//if the pov is pressed on the top the arm is to move up
+	//if the pov is pressed on the bottom the arm is to move down
+		//otherwise if the pov is not pressed at th certain angles the arm is to stop
 
 	/**
 	 * Direction constants for the horizontal motor.
@@ -61,28 +113,34 @@ public class Claw {
 		OPEN(1.0),
 		STOP(0.0),
 		CLOSED(-1.0);
+		//constants used to label OPEN, CLOSED, and STOP
 
 		private double speed;
 		private HorizontalDirection(double speed) { this.speed = speed; }
+		//inputing a HorizontalDirection with a speed
 
 		/**
 		 * Returns the speed the horizontal motor should spin at to move in the
 		 * requested direction.
 		 * @return The desired motor speed.
 		 */
+		//
 		public double getSpeed() { return this.speed; }
 	}
+		//returns the speed back to its original speed
 
 	/**
 	 * Direction constants for the vertical motor.
 	 */
 	public static enum VerticalDirection {
-		UP(1.0),
+		UP(-1.0),
 		STOP(0.0),
-		DOWN(-1.0);
+		DOWN(1.0);
+		//constants used to label UP, DOWN, and STOP
 
 		private double speed;
 		private VerticalDirection(double speed) { this.speed = speed; }
+		//inputing a VerticalDirection with a speed
 
 		/**
 		 * Returns the speed the horizontal motor should spin at to move in the
@@ -90,5 +148,6 @@ public class Claw {
 		 * @return The desired motor speed.
 		 */
 		public double getSpeed() { return this.speed; }
+		//retuens the speed back to its originakl speed
 	}
 }
--- org/usfirst/frc/team2264/robot/Robot.java
+++ org/usfirst/frc/team2264/robot/Robot.java
@@ -20,26 +20,49 @@ public class Robot extends IterativeRobot {
 	 */
 	public void robotInit() {
 		// Initialize the claw.
-		// TODO
-
+	
+		DigitalInput DI = new DigitalInput(RobotParts.CLAW_SAFETY_SWITCH);
+		CANTalon horizontalMotor = new CANTalon(RobotParts.MOTORS.CLAW_HORIZONTAL);		
+		CANTalon verticalMotor = new CANTalon(RobotParts.MOTORS.CLAW_VERTICAL);
+		this.claw = new Claw(horizontalMotor, verticalMotor, DI);
+		
 		// Initialize the drive train.
-		// TODO
+		this.drive = new RobotDrive(RobotParts.MOTORS.FRONT_LEFT, RobotParts.MOTORS.FRONT_RIGHT, RobotParts.MOTORS.REAR_LEFT, RobotParts.MOTORS.REAR_RIGHT);
+		
+		
+		 
+		
 
 		// Initialize the joystick.
-		// TODO
+		this.joystick = new Joystick(0);
+		
 	}
 
 	/**
 	 * This function is called periodically during the teleop period.
 	 */
 	public void teleopPeriodic() {
-		// Get the axes, "safety button", and POV from the joystick.
-		// TODO
-
+		
+		this.joystick.getRawAxis(RobotParts.AXES.X);
+		this.joystick.getRawAxis(RobotParts.AXES.Y);
+		this.joystick.getRawAxis(RobotParts.AXES.TWIST);
+		this.joystick.getRawButton(RobotParts.SAFETY_DISABLE_BUTTON);
+		this.joystick.getPOV();
+		
+	
+	
 		// Set the drive motors as indicated by the joystick.
-		// TODO
+		this.drive.mecanumDrive_Cartesian(RobotParts.AXES.X,RobotParts.AXES.Y,RobotParts.AXES.TWIST,0);
+		
+		
 
 		// Set the movement for the claw mechanism.
-		// TODO
+		this.claw.setVerticalDirection(this.joystick.getPOV());
+		this.claw.setHorizontalDirection(this.joystick.getPOV(), joystick.getRawButton(RobotParts.SAFETY_DISABLE_BUTTON));
+		
+		//this. has a claw. The claw in this. uses the getPOV from the joystick object to set the
+		//vertical direction of the motor for the claw.
+		//Not only does this have to get getPOV, it also has to manage the safety buttom. 
+		
 	}
 }
